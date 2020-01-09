d83ebf34b8465591891a14dca4029472493b3175
Cinzya <cynti-ebert@web.de>
2020-01-09 16:42:32 +0100

    added fetch call

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import RssListScreen from './screens/RssListScreen';
 
 export default function App() {
   return (
--- screens/RssListScreen.js
+++ screens/RssListScreen.js
@@ -1,6 +1,40 @@
 import React, { Components } from 'react';
 
 class RssListScreen extends React {
+    constructor() {
+        super();
+        state = {
+            rssURLs: [
+                {
+                    PiaproBlog: 'blog.piapro.net/feed'
+                },
+                {
+                    VNN: 'https://www.vocaloidnews.net/feed/'
+                },
+                {
+                    paraisovocaloid: 'https://paraisovocaloid.wordpress.com/feed/'
+                }
+            ]
+            rssList: []
+        }
+    }
+
+    componentDidMount(url) {
+        RSScall.map(
+            fetch("https://api.rss2json.com/v1/api.json?rss_url=" + url {
+                method: 'GET'
+            })
+            .then(response => {
+                return response.json()
+            })
+            .then(responseData => {
+                this.setState(state => {
+                    return {rssList: state.rssList.concat(responseData)}
+                })
+            })
+            
+        )
+    }
 
 }
 
